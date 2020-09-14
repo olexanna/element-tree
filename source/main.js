f49a04d54c00b82d0f3d4531f3c7740cb26d7119
Kengarut <kengarut@gmail.com>
2020-09-15 01:28:25 +0300

    ~

--- source/main.js
+++ source/main.js
@@ -1,152 +1,176 @@
 import "@styles/main.scss"
-/*
-	getList(){
+import "@styles/tree.scss"
 
-		fetch("/assets/requests/list.json")
-		.then( response => response.json() )
-		.then( data => {
+class Element{
 
-			for( let key in data ){
-				const elem = data[ key ];
-				this.add({
-					id: elem.id,
-					title: elem.title,
-					order: elem.srt
-				}, elem.parent_id );
-			};
+	constructor(){
+		this.name = "";
+		this.id = Element.count++;
+		this.parent = null;
+		this.children = [];
+		this.table = {};
+		this.isExpanded = false;
 
-		}).catch( error => {
+		this.ui = {
+			wrap: document.createElement( "div" ),
+			expand: document.createElement( "div" ),
+			name: document.createElement( "div" ),
+			add: document.createElement( "div" ),
+			remove: document.createElement( "div" ),
+			children: document.createElement( "div" )
+		};
+
+		this.ui.wrap.className = "tree-element";
+		this.ui.expand.className = "tree-element-expand tree-element-expand-closed";
+		this.ui.name.className = "tree-element-name";
+		this.ui.add.className = "tree-element-add";
+		this.ui.remove.className = "tree-element-remove";
+		this.ui.children.className = "tree-element-children tree-element-children-closed";
+
+		this.ui.wrap.append( this.ui.name );
+		this.ui.wrap.append( this.ui.expand );
+		this.ui.wrap.append( this.ui.add );
+		this.ui.wrap.append( this.ui.remove );
+		this.ui.wrap.append( this.ui.children );
+
+		this.ui.expand.addEventListener( "click", ( event ) => {
+
+			if( this.isExpanded )
+				this.reduce();
+			else
+				this.expand();
+
+		});
+		this.ui.add.addEventListener( "click", ( event ) => {
+			let element = new Element();
+			element.rename( "Name: " + element.id );
+			this.expand();
+			this.add( element );
+		});
+		this.ui.remove.addEventListener( "click", ( event ) => {
+
+			if( !this.parent )
+				return;
+
+			this.parent.remove( this );
 
 		});
 
-		return;
-	};
- */
-
-class Tree{
-
-
-	constructor(){
-		this.main = document.getElementById( "main" );
-	}
-
-
-	rootText(){
-		this.rootTitle =  document.createElement( "p" );
-		this.rootTitle.className = "root-title";
-		this.rootTitle.innerText = "Choose your destiny";
-		this.root.appendChild(this.rootTitle );
-	}
-
-	rootButtons(){
-
-		this.btn={
-			addElem: document.createElement( "p" ),
-			deleteElem:document.createElement( "p" )
-		};
-
-		this.btn.addElem.className = "root-add";
-		this.btn.addElem.innerText = "Add Element";
-
-		this.btn.deleteElem.className = "root-remove";
-		this.btn.deleteElem.innerText = "Remove Element";
-
-		for( let key in this.btn ){
-
-			this.btn.elems = this.btn[ key ];
-			this.root.appendChild( this.btn.elems );
-		}
-
-		this.btn.addElem.addEventListener("click", () =>this.addLine() );
-		this.btn.deleteElem.addEventListener("click", () =>this.deleteLine() );
-	}
-
-	addLine(){
-		this.line = document.createElement( "p" );
-		this.line.className = "line";
-		this.blockElems.appendChild( this.line );
-	}
-
-	deleteLine(){
-
-		this.blockElems.removeChild(this.blockElems.childNodes[0]);
-		console.log(this.blockElems.childNodes);
-	}
-
-	start(){
-
-		this.root = document.createElement( "div" );
-		this.root.className = "root";
-
-		this.blockElems =  document.createElement( "div" );
-		this.blockElems.className = "block-elem";
-		this.root.appendChild( this.blockElems );
-
-		this.rootText();
-		this.rootButtons();
-
-		this.main.appendChild( this.root );
-
-		console.log( this.main );
-	}
-
-
-}
-//
-//let tree = new Tree();
-//tree.start();
-
-class Branch{
-
-	constructor(){
-		this.elem = document.createElement( "div" );
-		this.parent = null;
-		this.list = [1,2,3,4];
 	};
 
-	add(){
-		let branch = new Branch();
-		branch.parent = this;
-		this.list.push( branch );
-	};
-	removeChild( branch ){
-
-		if( !branch )
-			return;
+	index( needle ){
 
 		let index = -1;
 
-		for( let child of this.list ){
+		for( const element of this.children ){
 
 			index++;
 
-			if( child.elem === branch.elem ){
-				branch.elem.remove();
-				this.list.splice( index, 1 );
-			}
+			if( element.id === needle.id )
+				return index;
 
-		}
+		};
 
+		return -1;
 	};
-	remove(){
+	add( element ){
 
-		if( !this.parent )
+		if( this.index( element ) > -1 )
 			return;
 
-		this.parent.removeChild( this );
+		this.children.push( element );
+		element.parent = this;
+
+		//#DOM
+		this.ui.children.append( element.ui.wrap );
 
 	};
-	rename(){};
-	expand(){};
-	reduce(){};
-}
-class Tree2{
+	remove( element ){
 
-	constructor(){
-		this.branch = new Branch();
+		const index = this.index( element );
+
+		if( index === -1 )
+			return;
+
+		this.children.splice( index, 1 );
+		element.parent = null;
+
+		//#DOM
+		element.ui.wrap.remove();
+
+	};
+	rename( name ){
+
+		this.name = name || "";
+
+		//#DOM
+		this.ui.name.innerHTML = this.name;
+
+	};
+	expand(){
+		this.ui.children.classList.remove( "tree-element-children-closed" );
+		this.ui.expand.classList.remove( "tree-element-expand-closed" );
+		this.isExpanded = true;
+	};
+	reduce(){
+		this.ui.children.classList.add( "tree-element-children-closed" );
+		this.ui.expand.classList.add( "tree-element-expand-closed" );
+		this.isExpanded = false;
+	};
+	getList( root, treeTable ){
+
+		fetch( "/assets/requests/list.json" )
+			.then( response => response.json() )
+			.then( data => {
+
+				let array = [];
+
+				for( let key in data ){
+
+					const params = {
+						id: data[ key ].id,
+						name: data[ key ].title,
+						order: data[ key ].srt,
+						parent: data[ key ].parent_id
+					};
+					array.push( params );
+
+					if( !treeTable[ params.id ] )
+						treeTable[ params.id ] = new Element();
+
+					treeTable[ params.id ].rename( params.name );
+
+				};
+
+				array.sort(function( a, b ){
+					return a.order - b.order;
+				});
+
+				for( let key in array ){
+
+					const params = array[ key ];
+					let element = treeTable[ params.id ];
+					let parent = treeTable[ params.parent ];
+
+					if( parent ){
+						parent.add( element );
+					}else{
+						root.add( element );
+					};
+
+				};
+
+			}).catch( error => {
+
+		});
+
 	};
 
 };
+Element.count = 0;
 
-let tree = new Tree2();
-document.body.appendChild( tree.branch.elem );
+let tree = new Element();
+tree.rename( "Tree" );
+tree.expand();
+tree.getList( tree, tree.table );
+document.body.append( tree.ui.wrap );
